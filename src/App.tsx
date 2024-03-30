5d617adc44ea9658b506539e66e557b2e28bed93
Bartłomiej Skorupski <bartmann001@gmail.com>
2024-03-30 17:22:41 +0100

    add mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import {
+  Button,
+  Card,
+  CardHeader,
+  IconButton,
+  Slider,
+  Tooltip,
+} from '@mui/material';
 import './App.css';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 function App() {
   return (
-    <>
+    <main className="flex flex-col gap-2 items-center">
       <h1 className="text-3xl md:text-5xl">Tag Browser</h1>
-    </>
+      <Button variant="contained">Test button</Button>
+      <Card className="px-4">
+        <CardHeader title="Some slider" />
+        <Slider />
+      </Card>
+      <Tooltip title="Delete">
+        <IconButton>
+          <DeleteIcon />
+        </IconButton>
+      </Tooltip>
+    </main>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,12 @@
 
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  important: '#root',
   theme: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 };
