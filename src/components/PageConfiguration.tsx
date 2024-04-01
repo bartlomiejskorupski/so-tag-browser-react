cab96a67fa1589ffc01b1e406ea96472c598e9e0
Bartłomiej Skorupski <bartmann001@gmail.com>
2024-04-01 02:07:45 +0200

    page configuration component

--- src/components/PageConfiguration.tsx
+++ src/components/PageConfiguration.tsx
@@ -1,9 +1,33 @@
-import { Paper, TextField } from '@mui/material';
+import { Button, Paper, TextField } from '@mui/material';
+import { ChangeEvent, useCallback, useRef, useState } from 'react';
 
 export default function PageConfiguration() {
+  const [value, setValue] = useState('10');
+  const textFieldRef = useRef<HTMLInputElement>();
+
+  const handleInputChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const newValue = Math.abs(+e.target.value);
+    setValue(`${newValue}`);
+  }, []);
+
+  const handleApplyClick = useCallback(() => {
+    console.log(textFieldRef.current?.value);
+  }, []);
+
   return (
-    <Paper>
-      <TextField type="number" label="Pages" />
+    <Paper className="max-w-fit py-3 px-4 mb-2">
+      <div className="flex items-center gap-2">
+        <TextField
+          inputRef={textFieldRef}
+          type="number"
+          label="Rows per page"
+          onChange={handleInputChange}
+          value={value}
+        />
+        <Button variant="contained" onClick={handleApplyClick}>
+          Apply
+        </Button>
+      </div>
     </Paper>
   );
 }
